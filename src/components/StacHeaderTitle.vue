47dc244aa239180de9f2c55b4045b318aa6107a3
Corentin Junod <corentin.junod@ne.ch>
2025-03-04 14:12:36 +0100

    Add sitn header banner

---
+++ src/components/StacHeaderTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header-title">
+    <div class="logo">{{ displayCatalogTitle }}</div>
+    <div class="text">
+      <a href="https://sitn.ne.ch">SITN<span class="text-full-name"> – Système d'Information du Territoire Neuchâtelois</span></a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'StacHeaderTitle',
+  components: {},
+  computed: {
+    ...mapGetters(['displayCatalogTitle']),
+  },
+  methods: {
+  }
+};
+</script>
